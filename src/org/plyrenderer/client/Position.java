a8018dea62c08adb3222c521c37305789ac6a48d
jfontignie <jacques.fontignie@gmail.com>
2012-05-23 11:56:08 +0200

    add copyrights

--- src/org/plyrenderer/client/Position.java
+++ src/org/plyrenderer/client/Position.java
@@ -1,3 +1,15 @@
+/*
+ * Copyright 2012 Jacques Fontignie
+ *
+ * This file is part of plyrenderer.
+ *
+ * plyrenderer is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * plyrenderer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with plyrenderer. If not, see http://www.gnu.org/licenses/.
+ */
+
 package org.plyrenderer.client;
 
 /**
